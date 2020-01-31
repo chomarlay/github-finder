0da8d229250d871d983be33753632f2f0f05b365
chomarlay <chomarlay.aung@gmail.com>
2020-02-01 00:32:44 +1100

    Section 4 - Alert state and component

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 import axios from 'axios';
 import Search from './components/layout/Search';
 
+import Alert from './components/layout/Alert';
+
 class App extends Component {
-  state = { users: [], loading: false };
+  state = { users: [], loading: false, alert: null };
 
   // async componentDidMount() {
   //   console.log('No Plan B');
@@ -27,17 +29,23 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   };
 
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, alert } = this.state;
 
     return (
       <div className='App'>
         <Navbar title=' No Plan B - Github Finder' />
+        <Alert alert={alert} />
         <div className='container'>
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
